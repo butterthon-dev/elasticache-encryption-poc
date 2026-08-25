e5913a175cff58cb208eccb4dc7832519d36de9d
daiki.tabata <daiki.tabata@datafluct.com>
2026-08-25 10:05:41 +0900

    ロギングの設定を追加

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,6 +130,20 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
